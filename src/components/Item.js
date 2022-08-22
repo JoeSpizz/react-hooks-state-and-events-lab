7d2d0445a90e913b9a3dafd994854fcc960ee1c3
Joe <spizjo01@gmail.com>
2022-08-22 09:25:21 -0600

    commit tree 055550ef7eca84c52a3bd1dc9ca5d6d782b6774e

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [clickStatus, setClick] = useState("")
+  
+  function itemClick(event){
+    let itemCart = event.target
+    let itemClicked = event.target.parentElement
+    setClick(()=> {
+      if (clickStatus === ""){
+        setClick ("in-cart")
+        itemClicked.className="in-cart"
+       itemCart.innerText = "Remove from Cart"
+
+      }
+      else if (clickStatus === "in-cart"){
+        setClick("")
+        itemClicked.className=""
+        itemCart.innerText = "Add to Cart"
+      }
+    })
+    }
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={itemClick} className="add">Add to Cart</button>
     </li>
   );
 }
